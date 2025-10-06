d3ea775a390b1a634fcb20683eea5e9427e17620
SynergyShadow <36507966+ShadowTigerCool@users.noreply.github.com>
2025-10-06 14:08:54 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.moulberry.axiom"
-version = "5.0.2+1.21.8"
+version = "5.0.2+1.21.9"
 description = "Serverside component for Axiom on Paper"
 
 java {
@@ -39,7 +39,7 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.21.8-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.9-R0.1-SNAPSHOT")
     implementation(libs.reflection.remapper)
     implementation(libs.cloud.paper)
 
